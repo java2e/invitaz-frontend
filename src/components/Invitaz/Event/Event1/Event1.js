74dbf14977dddc095b863468b5271801e8db8473
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-02-15 17:15:06 +0300

    Update Event1.js
    add components

--- src/components/Invitaz/Event/Event1/Event1.js
+++ src/components/Invitaz/Event/Event1/Event1.js
@@ -1,19 +1,55 @@
 import React from 'react';
+import { InputGroup,FormControl,Card,Button,Form} from 'react-bootstrap';
+
+import Men from '../../../../assets/images/masculine.png';
+import Women from '../../../../assets/images/femenine.png';
+
+
 
 
 const event1 = (props) => {
-    return(
+    return (
 
         <div>
-            <div className="ui input">
-                <input type="text"  placeholder="AD SOYAD" style={{width:600,textAlign:'center',height:50,fontSize:30}}/>
-            </div>
-            <div style={{marginTop:30}}></div>
-            <div className="ui input">
-                <input type="text"  placeholder="AD SOYAD" style={{width:600,textAlign:'center',height:50,fontSize:30}}/>
-            </div>
-        </div>
 
+            <div style={{margin:'auto',width: '100%'}}>
+                <Form.Label>
+                    <img src={Women} alt="MyBurger"/>
+                </Form.Label>
+                <InputGroup className="mb-3"  style={{width: '100%', textAlign: 'center', fontSize: 30}}>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>KADIN</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        onChange={(event)=>props.changed(event,'adSoyad1')}
+                        value={props.form.adSoyad1}
+                        placeholder="Adı Soyadı"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </div>
+
+            <div style={{marginTop: 30}}></div>
+            <div style={{margin:'auto',width: '100%'}}>
+                <Form.Label>
+                    <img src={Men} alt="MyBurger"/>
+                </Form.Label>
+                <InputGroup className="mb-3"  style={{width: '100%', textAlign: 'center', fontSize: 30}}>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>ERKEK</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        onChange={(event)=>props.changed(event,'adSoyad2')}
+                        value={props.form.adSoyad2}
+                        placeholder="Adı Soyadı"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </div>
+
+        </div>
 
 
     );
