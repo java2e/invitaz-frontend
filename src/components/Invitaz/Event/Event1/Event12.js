7082388e16857e4f858dfd459d646124dbd2da39
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-02-15 17:15:27 +0300

    Update Event12.js

--- src/components/Invitaz/Event/Event1/Event12.js
+++ src/components/Invitaz/Event/Event1/Event12.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
+import {Form, InputGroup} from 'react-bootstrap';
+
+import DatePicker from "react-datepicker";
+
+import 'react-datepicker/dist/react-datepicker-cssmodules.css'
+
 
 const event12 = (props) => {
-    return(
+    return (
 
-        <div>
-            <div className="ui input">
-                <input type="text"  placeholder="AD SOYAD" style={{width:600,textAlign:'center',height:50,fontSize:30}}/>
+        <div style={{width:'100%',margin:'auto'}}>
+            <div style={{width:'50%',margin:'auto'}}>
+                <Form.Label>
+
+                </Form.Label>
+                <InputGroup className="mb-3" style={{fontSize: 30}}>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>TARİH</InputGroup.Text>
+                    </InputGroup.Prepend>
+
+                    <DatePicker
+                        selected={props.form.tarih}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="dd mm yyyy h:mm aa"
+                        timeCaption="time"
+                        onChange={props.onHandlerDate}
+                    />
+
+                </InputGroup>
             </div>
-            <div style={{marginTop:30}}></div>
+            <div style={{marginTop: 30}}></div>
         </div>
-
 
 
     );
