5780e7cf1ac910a5989c3d5d2b26005a57b573f4
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-03-08 17:19:32 +0300

    Create burgerBuilder.js

---
+++ src/store/reducers/burgerBuilder.js
@@ -0,0 +1,15 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4,
+    error: false
+};
+
+
+
+const reducer = ( state = initialState, action ) => {
+   return state;
+};
+
+export default reducer;
