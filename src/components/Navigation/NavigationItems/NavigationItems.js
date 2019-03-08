6a3b7b0b1eeb13054536e491060e25cafa853548
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-03-08 17:26:31 +0300

    Update NavigationItems.js

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,10 +3,16 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
+        <NavigationItem link="/">AnaSayfa</NavigationItem>
+        <NavigationItem link="/">Nasıl Çalışır?</NavigationItem>
+        <NavigationItem link="/signup">Üye Ol</NavigationItem>
+
+        {!props.isAuthenticated
+            ? <NavigationItem link="/login">Giriş Yap</NavigationItem>
+            : <NavigationItem link="/logout">Çıkış Yap</NavigationItem>}
+
     </ul>
 );
 
