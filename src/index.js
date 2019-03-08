0c2fd51e3435dd9d3edb7f5a8115c5c2cc9e3a4f
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-03-08 17:22:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import authReducer from './store/reducers/auth';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    auth:authReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+const app = (
+    <Provider store={store}>
+            <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
+
