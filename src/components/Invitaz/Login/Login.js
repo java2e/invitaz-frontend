a1681c84e06af8e6ae68f48dd59d275be4c2281a
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-03-12 17:26:19 +0300

    Update Login.js

--- src/components/Invitaz/Login/Login.js
+++ src/components/Invitaz/Login/Login.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
-import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
 import classes from "./Login.css";
 
-import * as actions from '../../../store/actions/index';
+import mainCss from '../../assets/login/css/main.css';
+import utilCss from '../../assets/login/css/util.css';
+import '../../assets/login/vendor/animate/animate.css';
+import '../../assets/login/vendor/css-hamburgers/hamburgers.min.css';
+import '../../assets/login/vendor/bootstrap/css/bootstrap.min.css';
+import '../../assets/login/vendor/select2/select2.min.css';
+import '../../assets/login/vendor/daterangepicker/daterangepicker.css';
+import '../../assets/login/fonts/iconic/css/material-design-iconic-font.min.css';
+
+import * as actions from '../../store/actions/index';
 
 import {connect} from 'react-redux';
 
@@ -42,34 +50,55 @@ import {connect} from 'react-redux';
     render() {
         return (
             <div className={classes.LoginContent}>
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="email" bsSize="large">
-                        <Form.Label>Email</Form.Label>
-                        <FormControl
-                            autoFocus
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="password" bsSize="large">
-                        <Form.Label>Password</Form.Label>
-                        <FormControl
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <Button
-                        block
-                        bsSize="large"
-                        disabled={!this.validateForm()}
-                        type="submit"
-                    >
-                        Login
-                    </Button>
-                </form>
-                <Button onClick={this.switchAuthHandler}>SWITCH TO {this.state.isSignUp ? 'SIGNIN':'SIGNUP'}</Button>
+
+                    <div className={mainCss["wrap-login100"]}>
+                        <form onSubmit={this.handleSubmit} className={mainCss["login100-form"]}>
+					<span className={mainCss["login100-form-title"]}>
+						Welcome
+					</span>
+                            <span className={mainCss["login100-form-title"]}>
+						<i className={"zmdi zmdi-font"}></i>
+					</span>
+
+                            <div className={mainCss["wrap-input100"]} data-validate="Valid email is: a@b.c">
+                                <input id="email" className={mainCss["input100"]}
+                                       type="text" name="email"
+                                       value={this.state.email}
+                                       onChange={this.handleChange}/>
+                                <span className={mainCss["focus-input100"]} data-placeholder="Email"></span>
+                            </div>
+
+                            <div className={mainCss["wrap-input100"]} data-validate="Enter password">
+						<span className={mainCss["btn-show-pass"]}>
+							<i className="zmdi zmdi-eye"></i>
+						</span>
+                                <input id="password" className={mainCss["input100"]}
+                                       type="password" name="pass"
+                                       value={this.state.password}
+                                       onChange={this.handleChange}/>
+                                <span className={mainCss["focus-input100"]} data-placeholder="Password"></span>
+                            </div>
+
+                            <div className={mainCss["container-login100-form-btn"]}>
+                                <div className={mainCss["wrap-login100-form-btn"]}>
+                                    <div className={mainCss["login100-form-bgbtn"]}></div>
+                                    <button className={mainCss["login100-form-btn"]} type="submit">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className={utilCss["text-center"]}>
+						<span className={mainCss.txt1}>
+							Don’t have an account?
+						</span>
+
+                                <a className={mainCss.txt2} href="#">
+                                    Sign Up
+                                </a>
+                            </div>
+                        </form>
+                    </div>
             </div>
         );
     }
