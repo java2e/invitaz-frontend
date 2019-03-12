d1397dd79984a7f08fed1437c38d1140c7e9b727
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-03-12 17:20:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import InvitazBuilder from "./containers/InvitazBuilder/InvitazBuilder";
 import {connect} from 'react-redux';
-import {BrowserRouter,withRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import * as actions from './store/actions/index';
 
 class App extends Component {
@@ -32,4 +32,3 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(null,mapDispatchToProps)(App);
-
