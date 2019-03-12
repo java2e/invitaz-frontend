7b9ecf1c97323544f2db1d7c097a2efe614ee316
Mehmet Demircioğlu <demircioglumhmt@gmail.com>
2019-03-12 17:27:48 +0300

    Update SignUp.js

--- src/components/Invitaz/Login/SignUp.js
+++ src/components/Invitaz/Login/SignUp.js
@@ -1,79 +1,144 @@
 import React, { Component } from "react";
-import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
 import classes from "./Login.css";
+import * as actions from "../../store/actions/index";
 
-export default class SignUp extends Component {
+import mainCss from '../../assets/login/css/main.css';
+import utilCss from '../../assets/login/css/util.css';
+import '../../assets/login/vendor/animate/animate.css';
+import '../../assets/login/vendor/css-hamburgers/hamburgers.min.css';
+import '../../assets/login/vendor/bootstrap/css/bootstrap.min.css';
+import '../../assets/login/vendor/select2/select2.min.css';
+import '../../assets/login/vendor/daterangepicker/daterangepicker.css';
+import '../../assets/login/fonts/iconic/css/material-design-iconic-font.min.css';
+
+import {connect} from 'react-redux';
+
+class SignUp extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            email: "",
-            password: ""
+            form : {
+                name:"",
+                username:"",
+                email: "",
+                password: "",
+                passwordConfirm:""
+            }
         };
     }
 
     validateForm() {
-        return this.state.email.length > 0 && this.state.password.length > 0;
+        return this.state.form.email.length > 0 && this.state.form.password.length > 0;
     }
 
     handleChange = event => {
+
+        console.log(event.target.id);
+
+        const data = {
+            ...this.state.form
+        }
+
+        data[event.target.id]=event.target.value;
+
+        console.log(data);
+
         this.setState({
-            [event.target.id]: event.target.value
+            form: data
         });
     }
 
     handleSubmit = event => {
         event.preventDefault();
-    }
+        const data = {
+            ...this.state.form
+        }
+        data['username']=data['email'];
+        console.log(data)
 
+        this.props.signUp(data);
+    }
     render() {
         return (
             <div className={classes.LoginContent}>
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="name" bsSize="large">
-                        <Form.Label>Ad Soyad</Form.Label>
-                        <FormControl
-                            autoFocus
-                            type="text"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="email" bsSize="large">
-                        <Form.Label>Email</Form.Label>
-                        <FormControl
-                            autoFocus
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="password" bsSize="large">
-                        <Form.Label>Password</Form.Label>
-                        <FormControl
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="passwordConfirm" bsSize="large">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <FormControl
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <Button
-                        block
-                        bsSize="large"
-                        disabled={!this.validateForm()}
-                        type="submit"
-                    >
-                        Login
-                    </Button>
-                </form>
+                <div className={mainCss["wrap-login100"]}>
+                    <form onSubmit={this.handleSubmit} className={mainCss["login100-form"]}>
+					<span className={mainCss["login100-form-title"]}>
+						Welcome
+					</span>
+                        <span className={mainCss["login100-form-title"]}>
+						<i className={"zmdi zmdi-font"}></i>
+					</span>
+
+                        <div className={mainCss["wrap-input100"]} data-validate="Valid email is: a@b.c">
+                            <input id="name" className={mainCss["input100"]}
+                                   type="text" name="name"
+                                   value={this.state.form.name}
+                                   onChange={this.handleChange}/>
+                            <span className={mainCss["focus-input100"]} data-placeholder="Name"></span>
+                        </div>
+
+                        <div className={mainCss["wrap-input100"]} data-validate="Valid email is: a@b.c">
+                            <input id="email" className={mainCss["input100"]}
+                                   type="text" name="email"
+                                   value={this.state.form.email}
+                                   onChange={this.handleChange}/>
+                            <span className={mainCss["focus-input100"]} data-placeholder="Email"></span>
+                        </div>
+
+
+                        <div className={mainCss["wrap-input100"]} data-validate="Enter password">
+						<span className={mainCss["btn-show-pass"]}>
+							<i className="zmdi zmdi-eye"></i>
+						</span>
+                            <input id="password" className={mainCss["input100"]}
+                                   type="password" name="pass"
+                                   value={this.state.form.password}
+                                   onChange={this.handleChange}/>
+                            <span className={mainCss["focus-input100"]} data-placeholder="Password"></span>
+                        </div>
+
+                        <div className={mainCss["wrap-input100"]} data-validate="Enter password">
+						<span className={mainCss["btn-show-pass"]}>
+							<i className="zmdi zmdi-eye"></i>
+						</span>
+                            <input id="passwordConfirm" className={mainCss["input100"]}
+                                   type="password" name="pass"
+                                   value={this.state.form.passwordConfirm}
+                                   onChange={this.handleChange}/>
+                            <span className={mainCss["focus-input100"]} data-placeholder="Password Confirm"></span>
+                        </div>
+
+                        <div className={mainCss["container-login100-form-btn"]}>
+                            <div className={mainCss["wrap-login100-form-btn"]}>
+                                <div className={mainCss["login100-form-bgbtn"]}></div>
+                                <button className={mainCss["login100-form-btn"]} type="submit">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className={utilCss["text-center"]}>
+						<span className={mainCss.txt1}>
+							Do have an account?
+						</span>
+
+                            <a className={mainCss.txt2} href="#">
+                               Sign In
+                            </a>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        signUp:(form) => dispatch(actions.signUp(form))
+    }
+}
+
+export default  connect(null,mapDispatchToProps) (SignUp);
